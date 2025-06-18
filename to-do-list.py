d7cc7e4304fb3bcb2164a342291b8e3a36bacb33
Samrat Khatri <khatrisamrat2005@gmail.com>
2025-06-18 18:02:52 +1000

    Logic and functionalties for code

--- to-do-list.py
+++ to-do-list.py
@@ -1,1 +1,16 @@
 
+# Making to-do list, will be using multiple files functions etc; OOP 
+# Add a 'Add task', 'View task', 'Mark taks as done', 'delete task', 'Clear completed taks', 'Categorise tasks', 'due date remindes'
+# Add tasks--> User enters text/taks and decription if needed--> Due date --> priority level of the taks
+# View taks--> show all tasks--> status done or not done--> priorty of the tasks and the due date
+# Mark as done -->user can selsect tasks and mark it as completed will show up completed or status completed in 'view tasks' when this happenes 
+# Delete taks--> user cna delete tasks
+# clear completed tasks--> all the completed or status done tasks can be removed from viw tasks and the databse 
+#categorise tasks--> categorise tasks in certain categories such as school, work etc; might have to add it to the  'add a task' part
+# due date and reminders-->track which takss are overdue and upcomng 
+#
+# Some thoughts: which functionalities tie into other functionalties, if mark as done should be integrated as seperate or into view tasks, same with delete tasks
+# Some thoughts: If i should have allfdunctionalites in view tasks ui (terminal), so only need add taks, view tasks and due date reminders ???
+#
+#
+
