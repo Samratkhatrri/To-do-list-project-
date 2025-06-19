60071257cd94c36d75365a1cbdf61c05d8a15d8f
Samrat Khatri <khatrisamrat2005@gmail.com>
2025-06-19 15:42:34 +1000

    added some additonal stuff

---
+++ view_task.py
@@ -0,0 +1,31 @@
+from add_task import Task
+
+
+# here i will implment the differen functionalties of theto do list and put them in the view task section/ module
+class ToDoList:
+    def __init__(self):
+        self.tasks= []
+
+
+    def add_task(self, add_task, priority_level, categorise, due_date):
+        new_task= Task(add_task, priority_level, categorise, due_date )
+        self.tasks.append(new_task)
+
+    def view_task():
+      
+        
+    def priority_level():
+        
+    def categorise():
+        
+    def mark_as_done():
+        
+    def delete_task():
+
+    def clear_completed_task():
+        
+    def due_date():
+
+
+
+
